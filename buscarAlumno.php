a8473c78853148de5e972ea13a2639a9207e1414
Zerotgen <75233272+DanielPerezVelez@users.noreply.github.com>
2022-07-07 13:00:48 -0500

    Add files via upload

---
+++ buscarAlumno.php
@@ -0,0 +1,23 @@
+<?php 
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET'){
+	require_once("conexion.php");
+	$id = $_GET['id'];
+	$query = "SELECT * FROM usuarios WHERE id = '$id'";
+	$result = $mysql->query($query);
+
+	if ($mysql->affected_rows > 0){
+		while ($row = $result->fetch_assoc())
+		{
+			$array = $row;
+		}
+		echo json_encode($array);
+	}
+	else
+	{
+		echo "not found any rows";
+	}
+	
+	$result->close();
+	$mysql->close();
+}
